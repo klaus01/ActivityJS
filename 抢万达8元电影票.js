721b2b21b3a6670bfb4d5f232a8c3f2d008547b2
klaus01 <kelei0017@gmail.com>
2013-05-10 15:25:07 +0900

    增加定时抢票功能

--- 抢万达8元电影票.js
+++ 抢万达8元电影票.js
@@ -7,24 +7,28 @@
 	2、设置好代码中的抢票影院ID。
 	3、打开上面的活动页面，等抢票时间还有几秒中的时候执行下面的JS代码。
 
-	财富广场店	= 305
-	SM  广场店	= 306
+	财富广场店		= 305
+	SM广场店		= 306
 	锦华万达广场店	= 307
-	金沙广场店	= 318
+	金沙广场店		= 318
 	红牌楼广场店	= 348
 	新都和顺广场店	= 831
 	金牛万达广场店	= 834
 */
 const C_CINEMA_ID = 307;//<--根据上面的影院列表设置你要抢的影院ID
+const C_BEGIN_HOURS = 10;//<--抢票时间点 小时（24小时制）
 const C_SUCCESS_MSG = "00";
 const C_ERROR_NOTLOGIN = "05"
-function qiangpiao() {
+function newQiangPiao() {
+	$("#sel_cinema").text($("#" + C_CINEMA_ID).text());
+	selCityCode = "4905168908";
+	var qp_success = false;
 	$.ajax({type:"POST", 
 		url:basePath+"christmasSecondSkill.do?",
 		data:"m=goSkill&cinemaId=" + C_CINEMA_ID + "&sid=" + Math.random(),
 		dataType:"text",
-		timeout:3000,
-		error:null,
+		timeout:10000,
+		error:function (XMLHttpRequest, textStatus, errorThrown) {},
 		success:function (msg) {
 			if (msg == C_SUCCESS_MSG)
 			{
@@ -36,10 +40,26 @@ function qiangpiao() {
 			else
 			{
 				console.log($("#cinema_wrapper2").children("#" + msg).text());
-				qiangpiao();
+				newQiangPiao();
 			}
 		}, 
-		complete:null
+		complete:function (XMLHttpRequest, textStatus) {}
 	});
 }
-qiangpiao();
+
+var vInterval = setInterval(function(){
+	var d1 = new Date();
+	var d2 = new Date();
+	d2.setHours(C_BEGIN_HOURS);
+	d2.setMinutes(0);
+	d2.setSeconds(0);
+	d2.setTime(d2.getTime() - 2000);
+	if (d1 > d2)
+	{
+		window.clearInterval(vInterval);
+		console.log("时间到了");
+		newQiangPiao();
+	}
+	else
+		console.log("时间没到");
+}, 1000);
