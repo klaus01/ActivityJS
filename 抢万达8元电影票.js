b64949942a04700a470a5ccfd134d0e24bd53014
klaus01 <kelei0017@gmail.com>
2013-05-17 12:24:42 +0800

    自动获取服务器时间。不再要求客户端时间与北京时间一致。

--- 抢万达8元电影票.js
+++ 抢万达8元电影票.js
@@ -3,10 +3,9 @@
 	http://www.wandafilm.com/christmasSecondSkill.do?m=getPreSuccessUsers
 	
 	使用方法：
-	1、校对本机时间与北京时间一致。http://open.baidu.com/special/time/
-	2、打开万达网站http://www.wandafilm.com/，登录你的万达账号，登录时选中自动登录。
-	3、设置好代码中的 抢票影院ID 和 抢票时间点。
-	4、打开活动页面http://www.wandafilm.com/christmasSecondSkill.do?m=getPreSuccessUsers，运行代码，之后就不用管了，到点后自动抢票。
+	1、打开万达网站http://www.wandafilm.com/，登录你的万达账号，登录时选中自动登录。
+	2、设置好代码中的 抢票影院ID 和 抢票时间点。
+	3、打开活动页面http://www.wandafilm.com/christmasSecondSkill.do?m=getPreSuccessUsers，运行代码，之后就不用管了，到点后自动抢票。
 	注意：一个账号一天只能抢一次，不管你有没有抢到。
 */
 
@@ -20,8 +19,8 @@
 	新都和顺广场店	= 831
 	金牛万达广场店	= 834
 */
-const C_CINEMA_ID = 307;//<--根据上面的影院列表设置你要抢的影院ID
-const C_BEGIN_HOURS = 10;//<--抢票时间点 小时（24小时制，现在有三个时间点10、12、14）
+const C_CINEMA_ID = 834;//<--根据上面的影院列表设置你要抢的影院ID
+const C_BEGIN_HOURS = 12;//<--抢票时间点 小时（24小时制，现在有三个时间点10、12、14）
 const C_SUCCESS_MSG = "00";
 const C_ERROR_NOTLOGIN = "05"
 function newQiangPiao() {
@@ -51,19 +50,31 @@ function newQiangPiao() {
 		complete:function (XMLHttpRequest, textStatus) {}
 	});
 }
+function GetServerTime() {
+	var http = new XMLHttpRequest;
+	http.open("HEAD", ".", false);
+	http.send(null);
+	return new Date(http.getResponseHeader("Date"));
+}
+
+var gBeginTime = new Date();
+	gBeginTime.setHours(C_BEGIN_HOURS);
+	gBeginTime.setMinutes(0);
+	gBeginTime.setSeconds(0);
+	gBeginTime.setTime(gBeginTime.getTime() - 7000);
+
+var gBT = new Date();
+var gServerTime = GetServerTime();
+var gET = new Date();
+	gServerTime.setTime(gServerTime.getTime() + (gET.getTime() - gBT.getTime()));
 
 var vInterval = setInterval(function(){
-	var d1 = new Date();
-	var d2 = new Date();
-	d2.setHours(C_BEGIN_HOURS);
-	d2.setMinutes(0);
-	d2.setSeconds(0);
-	d2.setTime(d2.getTime() - 2000);
-	if (d1 > d2)
+	gServerTime.setTime(gServerTime.getTime() + 1000);
+	if (gServerTime >= gBeginTime)
 	{
+		newQiangPiao();
 		window.clearInterval(vInterval);
 		console.log("时间到了");
-		newQiangPiao();
 	}
 	else
 		console.log("时间没到");
