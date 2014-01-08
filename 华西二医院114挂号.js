66da9814dea881e43a37cdfcdfe09bb27a7f83bd
klaus01 <kelei0017@gmail.com>
2014-01-08 22:14:08 +0800

    新版本
    直接显示出验证码，然后输入验证码、预约日期、就诊卡号，之后就自动预约。

--- 华西二医院114挂号.js
+++ 华西二医院114挂号.js
@@ -1,129 +1,109 @@
-/* 查看验证码
-
-
-document.getElementById("img1");
-
-
-*/
-
-
-const C_VerificationCode 	= "5041"; 					//验证码
-const C_GHDate 				= "2014-02-11"; 			//预约日期
-const C_hosCard 			= "0001881321"; 			//就诊卡号。测试号：0001881318
-
 // 获取Url参数值
-function getUrlParam(paramName) {
-	var urlParamsStr = location.search.substring(1);
-	var pararmArray = urlParamsStr.split("&");
-	if (pararmArray != null)
-		for (var i = 0; i < pararmArray.length; i++) {
-			var p1Array = pararmArray[i].split("=");
-			if (p1Array[0] == paramName)
-				return decodeURI(p1Array[1]);
-		}
-	 return null;
+function getUrlParam(a) {
+    var b, c, d = location.search.substring(1), e = d.split("&");
+    if (null != e) for (b = 0; b < e.length; b++) if (c = e[b].split("="), c[0] == a) return decodeURI(c[1]);
+    return null;
 }
-
-const C_hosid  = getUrlParam("hosid");//医院ID
-const C_deptid = getUrlParam("deptid");//科室名称
-const C_docid  = getUrlParam("docid");//医生名称
-
-var gWorkID = "";
-var gCardInfoJson = null;
-var gRefreshPBInterval = null;
-
-// 取就诊卡信息
-$.ajax({
-    url: "Action/Valid_SC.ashx",
-    data: {
-        "d": new Date().getTime(),
-        "hosNo": C_hosid,
-        "hosCard": C_hosCard
-    },
-    success: function(htmlstr) {
-        console.log("就诊卡信息获取完成" + htmlstr);
-        gCardInfoJson = $.parseJSON(htmlstr);
-    }
-});
-
+// 显示日志
+function myLog(a) {
+    var b = new Date().toLocaleString();
+    gLogObj.innerHTML = b + "  " + a + "\r\n" + gLogObj.innerHTML;
+}
 // 刷新医生排班信息
 function RefreshPBInfo() {
     $.ajax({
-        url: "Action/GetTheArrange.ashx",
-        data: {
-            "d": new Date().getTime(),
-            "hosid": C_hosid,
-            "deptid": C_deptid,
-            "docid": C_docid
+        url:"Action/GetTheArrange.ashx",
+        data:{
+            d:new Date().getTime(),
+            hosid:C_hosid,
+            deptid:C_deptid,
+            docid:C_docid
         },
-        success: function(htmlstr) {
-            //console.log(htmlstr);
-            var regStr = '<a [^>]*id="a_' + C_hosid + '_([^"]*)[^>]*">' + C_GHDate;
-            var reg = new RegExp(regStr);
-            var r = htmlstr.match(reg);
-            if (r != null) {
-                gWorkID = r[1];
-                console.log("找到日期了，workid=" + gWorkID);
-                
+        success:function(a) {
+            var b = '<a [^>]*id="a_' + C_hosid + '_([^"]*)[^>]*">' + gGHDate.value, c = new RegExp(b), d = a.match(c);
+            null != d ? (gWorkID = d[1], myLog("找到日期了，workid=" + gWorkID), Registered(), setInterval(function() {
                 Registered();
-                setInterval(function() {
-                    Registered();
-                },
-                1000);
-                
-                window.clearInterval(gRefreshPBInterval);
-            }
-            else console.log("没找到指定日期" + regStr);
+            }, 1e3), window.clearInterval(gRefreshPBInterval)) :myLog("没找到指定日期" + b);
         }
     });
 }
-gRefreshPBInterval = setInterval(function() {
-    RefreshPBInfo()
-},
-2000);
-
 // 预约挂号
 function Registered() {
-    if (gCardInfoJson == null) {
-        console.log("就诊卡信息为空");
-        return;
-    }
-    $.ajax({
-        url: "Action/Reg_SC.ashx",
-        data: {
-            "d": new Date().getTime(),
-            "HosNo": C_hosid,
-            "workId": gWorkID,
-            "name": gCardInfoJson.UserName,
-            "sex": gCardInfoJson.UserSex,
-            "tel": gCardInfoJson.UserTel,
-            "address": gCardInfoJson.UserAddress,
-            "IdCard": gCardInfoJson.UserIdCard,
-            "age": gCardInfoJson.UserAge,
-            "HospitalCard": gCardInfoJson.UserCode,
-            "SendPhone": gCardInfoJson.UserTel,
-            "deptName": C_deptid,
-            "doctorName": C_docid,
-            "dutyDate": C_GHDate,
-            "cardNo": 0,
-            "dutyTime": 1,
-            "rbTime": 0,
-            "codevalue": C_VerificationCode,
-            "excode": gCardInfoJson.code,
-            "useridkey": gCardInfoJson.useridkey
+    return null == gCardInfoJson ? (myLog("就诊卡信息为空"), void 0) :($.ajax({
+        url:"Action/Reg_SC.ashx",
+        data:{
+            d:new Date().getTime(),
+            HosNo:C_hosid,
+            workId:gWorkID,
+            name:gCardInfoJson.UserName,
+            sex:gCardInfoJson.UserSex,
+            tel:gCardInfoJson.UserTel,
+            address:gCardInfoJson.UserAddress,
+            IdCard:gCardInfoJson.UserIdCard,
+            age:gCardInfoJson.UserAge,
+            HospitalCard:gCardInfoJson.UserCode,
+            SendPhone:gCardInfoJson.UserTel,
+            deptName:C_deptid,
+            doctorName:C_docid,
+            dutyDate:gGHDate.value,
+            cardNo:0,
+            dutyTime:1,
+            rbTime:0,
+            codevalue:gVerificationCode.value,
+            excode:gCardInfoJson.code,
+            useridkey:gCardInfoJson.useridkey
         },
-        success: function(htmlstr) {
-            var jsons = $.parseJSON(htmlstr);
-            if (jsons) {
-                if (jsons.success == -1) {
-                    console.log("验证码错误！");
-                    return;
-                }
-                if (jsons.success == 1)
-                	window.location.href = "success_SC.aspx?json=" + htmlstr;
-                else
-                	console.log('预约失败！' + jsons._errorMsg);
+        success:function(a) {
+            var b = $.parseJSON(a);
+            if (b) {
+                if (-1 == b.success) return myLog("验证码错误！"), void 0;
+                1 == b.success ? window.location.href = "success_SC.aspx?json=" + a :myLog("预约失败！" + b._errorMsg);
             }
         }
-    });
+    }), void 0);
 }
+
+var gWorkID = "", gCardInfoJson = null, gRefreshPBInterval = null;
+//医院ID、科室名称、医生名称
+const C_hosid = getUrlParam("hosid"), C_deptid = getUrlParam("deptid"), C_docid = getUrlParam("docid");
+//把验证码显示出来
+document.body.appendChild(document.getElementById("img1"));
+//验证码输入框
+var gVerificationCode = document.createElement("input");
+gVerificationCode.type = "text";
+gVerificationCode.value = "";
+document.body.appendChild(gVerificationCode);
+//预约日期输入框
+var gGHDate = document.createElement("input");
+gGHDate.type = "text";
+gGHDate.value = "2014-02-11";
+document.body.appendChild(gGHDate);
+//就诊卡号输入框。测试号：0001881318 张秀清
+var gHosCard = document.createElement("input");
+gHosCard.type = "text";
+gHosCard.value = "0001881321";
+document.body.appendChild(gHosCard);
+//开始预约按钮
+var gBtnExecJS = document.createElement("input");
+gBtnExecJS.type = "button";
+gBtnExecJS.value = "开始预约";
+gBtnExecJS.onclick = function() {
+    $.ajax({
+        url:"Action/Valid_SC.ashx",
+        data:{
+            d:new Date().getTime(),
+            hosNo:C_hosid,
+            hosCard:gHosCard.value
+        },
+        success:function(a) {
+            myLog("就诊卡信息获取完成" + a), gCardInfoJson = $.parseJSON(a);
+        }
+    }), gRefreshPBInterval = setInterval(function() {
+        RefreshPBInfo();
+    }, 5e3);
+};
+document.body.appendChild(gBtnExecJS);
+//日志显示框
+var gLogObj = document.createElement("textarea");
+gLogObj.style.width = "700px", gLogObj.style.height = "300px", document.body.appendChild(gLogObj), 
+scroll(0, document.body.scrollHeight);
